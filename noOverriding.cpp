6e0f63bd4eea2984e370e8d016d710ddf57e9da4
ArgyTataMaulana <halmaheru37@gmail.com>
2025-06-03 18:46:31 +0700

    membuat baseClass dengan void perkenalan

---
+++ noOverriding.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+class baseClass {
+    public: 
+        virtual void perkenalan() {
+            cout << "Halo nama saya function dari base class";
+        }
+};
+

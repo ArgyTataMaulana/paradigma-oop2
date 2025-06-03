4dacc9411b02b45cbceddbf9ead282370e331e94
ArgyTataMaulana <halmaheru37@gmail.com>
2025-06-03 17:26:21 +0700

    membuat class lia dari seseorang dan berisikan fungsi void pesan

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -15,3 +15,10 @@ class Joko : public Seseorang {
             cout << "Pesan dari Joko" << endl;
         }
 };
+
+class lia :public Seseorang {
+    public:
+    void pesan() {
+        cout << "Pesan dari Lia" << endl;
+    }
+};

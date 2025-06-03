be114eb0b98401681fd27ec2207ac386d47c10ce
ArgyTataMaulana <halmaheru37@gmail.com>
2025-06-03 17:23:20 +0700

    membuat class joko dari seseorang dan berisikan fungsi void pesan

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 using namespace std;
 
-class seseorang {
+class Seseorang {
     public:
     virtual void pesan() = 0;
     //virtual void pesan() {
     //cout << "pesan dari seseorang" << endl;
     //}
+};
+
+class Joko : public Seseorang {
+    public:
+        void pesan() {
+            cout << "Pesan dari Joko" << endl;
+        }
 };
